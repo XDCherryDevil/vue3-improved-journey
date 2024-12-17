7abaa620bfc2923513526a0c3b2676aad75c964b
Cherry Devil XD <1459225984@qq.com>
2024-12-17 17:44:56 +0800

    Changes to be committed:

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';  // 引入 Element Plus 的 CSS
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).mount("#app"); 为什么报错
